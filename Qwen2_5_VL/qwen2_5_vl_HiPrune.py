32c684360415d3f770a178fcb86b036830421c45
mmdbhs <48610427+mmdbhs@users.noreply.github.com>
2025-09-02 03:22:36 +0000

    确保稀疏token数正确

--- Qwen2_5_VL/qwen2_5_vl_HiPrune.py
+++ Qwen2_5_VL/qwen2_5_vl_HiPrune.py
@@ -574,6 +574,7 @@ class Qwen2_5_VisionTransformerPretrainedModel(Qwen2_5_VLPreTrainedModel):
                     attn_weights = attn_weights.sum(dim=0) 
                     attn_weights = attn_weights.view(attn_weights.shape[0]//4, -1).mean(dim=-1)
                     attn_weights = attn_weights[reverse_indices]
+                    attn_weights = F.softmax(attn_weights)
                 attn_list.append(attn_weights)
         hidden_states = self.merger(hidden_states)
         hidden_states = hidden_states[reverse_indices, :]
